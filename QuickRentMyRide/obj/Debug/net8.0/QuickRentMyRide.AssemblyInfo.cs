fa2c943a352dee7ec3a5a29d5700a11b83185a4e
sarun2005 <sarun1994340@gmail.com>
2025-07-21 20:05:37 +0530

    first commit

--- QuickRentMyRide/obj/Debug/net8.0/QuickRentMyRide.AssemblyInfo.cs
+++ QuickRentMyRide/obj/Debug/net8.0/QuickRentMyRide.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuickRentMyRide")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ba3a1931ef3e60046be1f24e97dd1de12d897cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52ca8360eec78dc26f77cf7ab225ea4c393910a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuickRentMyRide")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuickRentMyRide")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
